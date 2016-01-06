ba4073bed9812ae330b1fce678e1d2bd91a5f90a
Paul Colomiets <paul@colomiets.name>
2016-01-06 19:15:30 +0200

    More structures added

--- src/name.rs
+++ src/name.rs
@@ -1,2 +1,7 @@
-pub struct Name<'a>(&'a [u8]);
+pub struct Name<'a>{
+    labels: &'a [u8],
+    /// This is the original buffer size. The compressed names in original
+    /// are calculated in this buffer
+    original: &'a [u8],
+}
 
--- src/structs.rs
+++ src/structs.rs
@@ -1,4 +1,13 @@
-use {QueryType, QueryClass, ResponseCode, Name};
+use std::net::Ipv4Addr;
+use {QueryType, QueryClass, ResponseCode, Name, Type, Class};
+
+pub enum RRData<'a> {
+    CNAME(Name<'a>),
+    A(Ipv4Addr),
+    SRV { priority: u16, weight: u16, port: u16, target: Name<'a> },
+    MX { prefererence: u16, exchange: Name<'a> },
+    Unknown(&'a [u8]),
+}
 
 pub struct RawMessage<'a> {
     pub header: &'a [u8],
@@ -28,6 +37,17 @@ pub struct Question<'a> {
     pub qclass: QueryClass,
 }
 
+pub struct RawResrouceRecord<'a> {
+    pub name: Name<'a>,
+    pub typ: Type,
+    pub cls: Class,
+    pub ttl: u32,
+    pub data: &'a [u8],
+}
+
 pub struct ResourceRecord<'a> {
     pub name: Name<'a>,
+    pub cls: Class,
+    pub ttl: u32,
+    pub data: RRData<'a>,
 }
