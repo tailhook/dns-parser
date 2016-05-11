e025aef3c96ae92912eb8d655a008b837d5c640c
Tenzin Rigden <tenzin.rigden@redjack.com>
2016-05-11 11:49:38 -0400

    Implement ptr-record parsing

--- src/rrdata.rs
+++ src/rrdata.rs
@@ -14,6 +14,7 @@ pub enum RRData<'a> {
     AAAA(Ipv6Addr),
     SRV { priority: u16, weight: u16, port: u16, target: Name<'a> },
     SOA(SoaRecord<'a>),
+    PTR(Name<'a>),
     MX { preference: u16, exchange: Name<'a> },
     // Anything that can't be parsed yet
     Unknown(&'a [u8]),
@@ -61,16 +62,8 @@ impl<'a> RRData<'a> {
                     exchange: try!(Name::scan(&rdata[2..], original)),
                 })
             }
-            Type::SRV => {
-                if rdata.len() < 7 {
-                    return Err(Error::WrongRdataLength);
-                }
-                Ok(RRData::SRV {
-                    priority: BigEndian::read_u16(&rdata[..2]),
-                    weight: BigEndian::read_u16(&rdata[2..4]),
-                    port: BigEndian::read_u16(&rdata[4..6]),
-                    target: try!(Name::scan(&rdata[6..], original)),
-                })
+            Type::PTR => {
+                Ok(RRData::PTR(try!(Name::scan(rdata, original))))
             }
             Type::SOA => {
                 let mut pos = 0;
@@ -90,6 +83,17 @@ impl<'a> RRData<'a> {
                     expire: BigEndian::read_u32(&rdata[(pos+12)..(pos+16)]),
                     minimum_ttl: BigEndian::read_u32(&rdata[(pos+16)..(pos+20)]),
                 }))
+            }
+            Type::SRV => {
+                if rdata.len() < 7 {
+                    return Err(Error::WrongRdataLength);
+                }
+                Ok(RRData::SRV {
+                    priority: BigEndian::read_u16(&rdata[..2]),
+                    weight: BigEndian::read_u16(&rdata[2..4]),
+                    port: BigEndian::read_u16(&rdata[4..6]),
+                    target: try!(Name::scan(&rdata[6..], original)),
+                })
             }
             _ => {
                 Ok(RRData::Unknown(rdata))
